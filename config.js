2aa11798826a9e9e85d97eb3b4d8db92ecfb29e7
tharumin <94857655+tharumin@users.noreply.github.com>
2026-01-13 03:08:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID === undefined ? 'SUHAS-MD=xOFhQD5R#2j2rkP8TzSPYDW6jue1T2kyOfuSWgn21Oymw7evVqIU' : process.env.SESSION_ID, //ADD YOUR SESSION ID
+    SESSION_ID: process.env.SESSION_ID === undefined ? 'SUHAS-MD=rMgB3aRA#Rzm6Kq_QhsMKGZEc9kH5BYDRLDO_n3DLF-pmKL4EmIA' : process.env.SESSION_ID, //ADD YOUR SESSION ID
     GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'Mohomad-Nisan' : process.env.GITHUB_USER_NAME, //ADD YOUR GITHUB USERNAME
     GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'adxPuaw6bpdAaBgn851IG82AEHsB4Q3zZGJu' : process.env.GITHUB_AUTH_TOKEN //ADD YOUR GITHUB AUTH TOKEN WITHOUT gph_ ,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
 };
