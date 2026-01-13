8b7a216d69fce364e45b998075c56d907b057ae3
tharumin <94857655+tharumin@users.noreply.github.com>
2026-01-13 18:59:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID === undefined ? 'SUHAS-MD=rMgB3aRA#Rzm6Kq_QhsMKGZEc9kH5BYDRLDO_n3DLF-pmKL4EmIA' : process.env.SESSION_ID, //ADD YOUR SESSION ID
-    GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'Mohomad-Nisan' : process.env.GITHUB_USER_NAME, //ADD YOUR GITHUB USERNAME
-    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'adxPuaw6bpdAaBgn851IG82AEHsB4Q3zZGJu' : process.env.GITHUB_AUTH_TOKEN //ADD YOUR GITHUB AUTH TOKEN WITHOUT gph_ ,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
+    GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'manpakaya: process.env.GITHUB_USER_NAME, //ADD YOUR GITHUB USERNAME
+    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'Thuz6U2HZHHPlLIVJqREyEo3m46XLF3sg1r5' : process.env.GITHUB_AUTH_TOKEN //ADD YOUR GITHUB AUTH TOKEN WITHOUT gph_ ,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
 };
